5f7ee22ae7822963ff01af0fad0040e886f8938e
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-06-02 13:06:33 +0500

    updated server

--- controllers/user.js
+++ controllers/user.js
@@ -518,18 +518,18 @@ const sendAdminConfirmationEmails = async (userEmail, firstName, password) => {
 
         <hr style="margin:40px 0;" />
 
-        <div style="text-align:center;">
+        <div style="text-align:center; display:flex;justify-content:center; align-items:center; width:100%; flex-direction:column;">
           <img src="${logoUrl}" alt="Logo" style="width:60px;height:60px;border-radius:50%;" />
           <p style="margin:10px 0 20px;">doTask Service Marketplace</p>
-          <div style="display:flex;justify-content:center;gap:15px;">
+          <div style="display:flex;justify-content:center;gap:15px; align-items:center; width:100%;">
             <a href="#" style="width:35px;height:35px;border:1px solid #ccc;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;text-decoration:none;">
-              <img src="https://img.icons8.com/ios-filled/20/000000/facebook-new.png" alt="Facebook"/>
+              <img src="https://img.icons8.com/ios-filled/20/000000/facebook-new.png" alt="Facebook" style="width:30px;" />
             </a>
             <a href="#" style="width:35px;height:35px;border:1px solid #ccc;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;text-decoration:none;">
-              <img src="https://img.icons8.com/ios-filled/20/000000/twitter.png" alt="Twitter"/>
+              <img src="https://img.icons8.com/ios-filled/20/000000/twitter.png" alt="Twitter" style="width:30px;" />
             </a>
             <a href="#" style="width:35px;height:35px;border:1px solid #ccc;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;text-decoration:none;">
-              <img src="https://img.icons8.com/ios-filled/20/000000/linkedin.png" alt="LinkedIn"/>
+              <img src="https://img.icons8.com/ios-filled/20/000000/linkedin.png" alt="LinkedIn" style="width:30px;" />
             </a>
           </div>
         </div>
